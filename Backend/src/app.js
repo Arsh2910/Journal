055589b458236628209f995a956bf5cb5151656a
Arsh <arshj347@gmail.com>
2026-08-28 18:32:02 +0530

    Fixed TYPO

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -15,7 +15,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:5174",
-      "https://daybook-one-swart.vercel.app/",
+      "https://daybook-one-swart.vercel.app",
     ],
     credentials: true,
   }),
